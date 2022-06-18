4ee89caec2cbd4b0c2944708a602a804c84b7e62
DanSandvig <dspublicaddress@protonmail.com>
2022-06-18 17:06:06 +0100

    Shifted navlinks to right

--- src/components/SiteNav.jsx
+++ src/components/SiteNav.jsx
@@ -7,7 +7,7 @@ const SiteNav = () => {
         <Navbar bg="dark" variant="dark">
             <Container>
                 <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                <Nav className="me-auto">
+                <Nav className="justify-content-end">
                     <Nav.Link href="#home">Home</Nav.Link>
                     <Nav.Link href="#features">Features</Nav.Link>
                     <Nav.Link href="#pricing">Pricing</Nav.Link>
